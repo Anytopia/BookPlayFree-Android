64fd4bd1aa2f8de3c7bdc8197e4975a446ba0595
Noor <luthfi.n57@gmail.com>
2025-04-02 18:46:18 +0700

    Adjust layout Home screen

--- feature/dashboard/src/main/java/com/zachnr/bookplayfree/dashboard/presentation/home/HomeScreen.kt
+++ feature/dashboard/src/main/java/com/zachnr/bookplayfree/dashboard/presentation/home/HomeScreen.kt
@@ -1,8 +1,11 @@
 package com.zachnr.bookplayfree.dashboard.presentation.home
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -14,11 +17,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.koin.androidx.compose.koinViewModel
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun HomeScreen(
     modifier: Modifier = Modifier,
@@ -28,56 +34,82 @@ internal fun HomeScreen(
     LaunchedEffect(Unit) {
         viewModel.getQuote()
     }
-    Box(
-        modifier = Modifier.fillMaxSize()
+    HomeScreen(
+        modifier = modifier,
+        state = state.value,
+        onExpandedChange = viewModel::setOnActiveStateSearchChanged,
+        onQueryChange = viewModel::setOnQuerySearchChanged
+    )
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun HomeScreen(
+    modifier: Modifier,
+    state: HomeState,
+    onQueryChange: (String) -> Unit = {},
+    onExpandedChange: (Boolean) -> Unit = {}
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 14.dp)
     ) {
         val colors1 = SearchBarDefaults.colors()
         SearchBar(
             inputField = {
                 SearchBarDefaults.InputField(
-                    query = state.value.searchQueryText,
-                    onQueryChange = { viewModel.setOnQuerySearchChanged(it) },
+                    query = state.searchQueryText,
+                    onQueryChange = { onQueryChange(it) },
                     onSearch = { /* Handle search */ },
-                    expanded = state.value.isSearchActive,
-                    onExpandedChange = { viewModel.setOnActiveStateSearchChanged(it) },
+                    expanded = state.isSearchActive,
+                    onExpandedChange = { onExpandedChange(it) },
                     enabled = true,
                     placeholder = { Text("Search") },
                     leadingIcon = { Icon(Icons.Default.Search, contentDescription = "Search") },
                     trailingIcon = null,
-                    colors = colors1.inputFieldColors,
                     interactionSource = null,
                 )
             },
-            expanded = state.value.isSearchActive,
-            onExpandedChange = { viewModel.setOnActiveStateSearchChanged(it) },
+            expanded = state.isSearchActive,
+            onExpandedChange = { onExpandedChange(it) },
             shape = SearchBarDefaults.inputFieldShape,
             colors = colors1,
             tonalElevation = SearchBarDefaults.TonalElevation,
             shadowElevation = SearchBarDefaults.ShadowElevation,
             windowInsets = SearchBarDefaults.windowInsets,
-            modifier = Modifier.align(Alignment.TopCenter)
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.CenterHorizontally)
         ) {
             // TODO: Add search result
         }
-        Content(state = state.value, modifier = Modifier.align(Alignment.Center))
+        Spacer(modifier = Modifier.height(18.dp))
+        Text(
+            text = "Welcome",
+            fontSize = 24.sp
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(
+            text = state.quote,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Light,
+            fontStyle = FontStyle.Italic
+        )
     }
+
 }
 
-@Composable
-private fun Content(
-    modifier: Modifier,
-    state: HomeState
-) {
-    Column(
-        modifier = modifier
-    ) {
-        Text(text = "Welcome")
-        Text(text = state.quote)
-    }
-}
-
-@Preview
+@Preview(
+    showBackground = true
+)
 @Composable
 private fun HomeScreenPreview() {
-    HomeScreen()
+    HomeScreen(
+        modifier = Modifier,
+        state = HomeState(quote = "\"A reader lives a thousand lives before he dies. The man who never reads lives only one.\" — George R.R. Martin"),
+        onQueryChange = {},
+        onExpandedChange = {}
+    )
 }
