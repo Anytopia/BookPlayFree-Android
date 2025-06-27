8b82525f230c0a7697b75fc0830112958d22d25a
Noor <luthfi.n57@gmail.com>
2025-06-28 00:56:10 +0700

    Add minimum fetch interval firebase to 1 hour

--- core/firebase/src/main/java/com/zachnr/bookplayfree/firebase/FirebaseModule.kt
+++ core/firebase/src/main/java/com/zachnr/bookplayfree/firebase/FirebaseModule.kt
@@ -4,13 +4,14 @@ import com.google.firebase.Firebase
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.remoteConfig
 import com.google.firebase.remoteconfig.remoteConfigSettings
+import com.zachnr.bookplayfree.utils.utils.FirebaseConst.MINIMUM_FETCH_INTERVAL
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 fun loadFirebaseModule() : Module = module {
     single<FirebaseRemoteConfig> {
         val configSettings = remoteConfigSettings {
-            minimumFetchIntervalInSeconds = 0
+            minimumFetchIntervalInSeconds = MINIMUM_FETCH_INTERVAL
         }
         Firebase.remoteConfig.apply {
             setConfigSettingsAsync(configSettings)
