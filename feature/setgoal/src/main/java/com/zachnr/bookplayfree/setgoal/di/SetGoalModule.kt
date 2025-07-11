68906e95eae144a1044ae6773bbf717505b39a0f
Noor <luthfi.n57@gmaill.com>
2025-07-11 22:41:56 +0700

    feat: resolve koin module not loaded

--- feature/setgoal/src/main/java/com/zachnr/bookplayfree/setgoal/di/SetGoalModule.kt
+++ feature/setgoal/src/main/java/com/zachnr/bookplayfree/setgoal/di/SetGoalModule.kt
@@ -3,12 +3,16 @@ package com.zachnr.bookplayfree.setgoal.di
 import com.zachnr.bookplayfree.setgoal.presentation.SetGoalViewModel
 import com.zachnr.bookplayfree.utils.utils.AppConst
 import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.core.module.Module
+import org.koin.core.context.loadKoinModules
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
-internal fun loadSetGoalModule(): Module {
-    return module {
-        viewModel { SetGoalViewModel(get(named(AppConst.APP_LEVEL_NAVIGATOR))) }
-    }
+internal fun loadSetGoalModule() {
+    loadKoinModules(
+        listOf(setGoalVMModule)
+    )
+}
+
+private val setGoalVMModule = module {
+    viewModel { SetGoalViewModel(get(named(AppConst.APP_LEVEL_NAVIGATOR))) }
 }
